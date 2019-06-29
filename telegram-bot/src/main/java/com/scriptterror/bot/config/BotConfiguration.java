1300f454fb90dfc3e19125eb1aa3c659a05ee80e
bobrutskovav <thirdstrike@yandex.ru>
2019-06-29 18:03:28 +0300

    multi modules

--- telegram-bot/src/main/java/com/scriptterror/bot/config/BotConfiguration.java
+++ telegram-bot/src/main/java/com/scriptterror/bot/config/BotConfiguration.java
@@ -3,13 +3,9 @@ package com.scriptterror.bot.config;
 
 import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
-import com.scriptterror.bot.Bot;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 
 import java.util.concurrent.Executors;
